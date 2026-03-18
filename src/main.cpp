b87e62a9336d106d9c949b7f1444c8358df4e0c2
Henri Gomez <henri.gomez@gmail.com>
2026-03-18 19:15:51 +0100

    Etape initiale

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <bluefruit.h>
+
+
+void setup() {
+  // Init serial communication for debugging
+  Serial.begin(115200);
+  long start = millis();
+  while ( !Serial && (millis() - start < 2000)) delay(10); 
+}
+
+
+void loop() {
+
+  // Manage bootloader switch for over_the_cable fw update (type ! on serial console)
+  while ( Serial && Serial.available() ) if (Serial.read()=='!') enterSerialDfu();
+
+  Serial.println("Hello Devoxx France");
+  delay(1000);
+}
